8257a76b8035e9b1fb51319183b04fa839550132
Ashok Mane <ashokmane81800@gmail.com>
2021-05-11 12:57:08 +0530

    [ashok] when we use wron csv file path it return custom exception

--- src/test/java/com/bridgelabz/IndianStateCensusAnalyzerTest.java
+++ src/test/java/com/bridgelabz/IndianStateCensusAnalyzerTest.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IndianStateCensusAnalyzerTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
@@ -20,4 +21,20 @@ public class IndianStateCensusAnalyzerTest {
             e.printStackTrace();
         }
     }
+
+    /**
+     * test case is use to when given wrong csv file return custom exception...
+     */
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
 }
+
